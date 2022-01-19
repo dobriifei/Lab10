bf248f39c1c9d28c9369434aa33c4faf6d22fbd5
vermei2@outlook.com <vermei2@outlook.com>
2022-01-19 21:41:25 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("введите значение угла");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            int minuta = Convert.ToInt32(Console.ReadLine());
+           int secunda = Convert.ToInt32(Console.ReadLine());
+            gradusi grad = new gradusi(gradus, minuta, secunda);
+            double radian = grad.ToRadians();
+            Console.WriteLine("{0}gr{1}'{2}'' равно {3:0.00}", gradus, minuta, secunda, radian);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab10/gradusi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class gradusi
+    {
+        int gradus;
+        int minuta;
+        int secunda;
+        public int Gradus
+        {
+            get 
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value;
+            }
+        } 
+        public int Minuta
+        {
+            get 
+            {
+                return minuta;
+            }
+            set
+            {
+                Gradus+=value/60;
+                minuta = value % 60;
+            }
+        }
+        public int Secunda
+        {
+            get 
+            {
+                return secunda;
+            }
+            set
+            {
+                Minuta+=value/60;
+                secunda = value % 60;
+            }
+        }
+        public gradusi(int gradus, int minuta, int secunda)
+        {
+            this.Gradus = gradus;
+            this.Minuta = minuta;
+            this.Secunda = secunda;
+        }
+        public double ToRadians()
+        {
+            return (gradus + (minuta + secunda / 60) / 60) * Math.PI / 180;
+        }
+    }
+}
